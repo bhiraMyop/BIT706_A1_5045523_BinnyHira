85f1f18b1c8cb760cf5f6231e558dc5ebc2d9c50
Binny Hira <BinnyHira@myop.ac.nz>
2023-04-15 16:39:01 +1200

    Created the Person Class and BanksTaff class

---
+++ BIT706_A1_5045523_BinnyHira/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIT706_A1_5045523_BinnyHira
+{
+    internal class Person
+    {
+        private int personID;
+        public int idCounter = 1;
+        private string name;
+        private string contactNo;
+        private string address;
+
+        public int PersonID { get => personID; set => personID = value; }
+        public string Name { get => name; set => name = value; }
+        public string ContactNo { get => contactNo; set => contactNo = value; }
+        public string Address { get => address; set => address = value; }
+        
+
+        public Person()
+        {
+            PersonID = idCounter;
+            idCounter++;
+        }
+        public Person(string name) : this()
+        {
+            Name = name;
+        }
+
+        public Person(string name,  string contactNo) : this(name)
+        {
+            Name = name;
+            ContactNo = contactNo;
+        }
+        public Person(string name, string contactNo, string address) : this(name, contactNo)
+        {
+            Name = name;
+            ContactNo = contactNo;
+            Address = address;
+        }
+
+
+
+    }
+}
