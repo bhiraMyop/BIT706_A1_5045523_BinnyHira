225c881bb25a9e3c80ed2223683a563c6a6c321c
Binny Hira <BinnyHira@myop.ac.nz>
2023-05-14 20:45:53 +1200

    ShowAccounts withdraw button click Try Catch block added

---
+++ BIT706_A1_5045523_BinnyHira/StringEnteredException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace BIT706_A1_5045523_BinnyHira
+{
+    [Serializable]
+    internal class StringEnteredException : Exception
+    {
+        public StringEnteredException()
+        {
+        }
+
+        public StringEnteredException(string message) : base(message)
+        {
+        }
+
+        public StringEnteredException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected StringEnteredException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
