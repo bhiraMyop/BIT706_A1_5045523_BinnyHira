3081c00ff47e606de150cbb61802ee3544ec191b
Binny Hira <BinnyHira@myop.ac.nz>
2023-05-27 21:53:51 +1200

    Intreest Rate is 4% on all accounts.

--- BIT706_A1_5045523_BinnyHira/EverydayAccount.cs
+++ BIT706_A1_5045523_BinnyHira/EverydayAccount.cs
@@ -94,7 +94,7 @@ namespace BIT706_A1_5045523_BinnyHira
 
         public override string toStringListAccounts()
         {
-            string str = $"Everyday Account {AccountNo}; Failed Fee ${FailedFee}; Balance ${Balance}";
+            string str = $"Everyday Account {AccountNo}; InterestRate {InterestRate}%; Failed Fee ${FailedFee}; Balance ${Balance}";
             return str;
         }
     }
